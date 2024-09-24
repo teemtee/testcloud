9bff4da6240dc171346da4ad114a69b10f71cead
Josef Skladanka <josef.skladanka@gmail.com>
2024-09-24 13:30:26 +0200

    Make sure an exception is raised on SIGTERM, to handle #191
    Fixes: #191

--- testcloud/__init__.py
+++ testcloud/__init__.py
@@ -1,2 +1,18 @@
 # NOTE: if you update version, *make sure* to also update `docs/source/conf.py`
-__version__ = '0.9.13'
+__version__ = "0.9.13"
+
+import signal
+import sys
+import threading
+
+
+def sigterm_handler(_signo, _stack_frame):
+    # A call to sys.exit() is translated into an exception so that clean-up handlers
+    #  (finally clauses of try statements) can be executed, and so that a debugger can
+    #  execute a script without running the risk of losing control.
+    sys.exit(0)
+
+
+
+if threading.current_thread() is threading.main_thread():
+    signal.signal(signal.SIGTERM, sigterm_handler)
