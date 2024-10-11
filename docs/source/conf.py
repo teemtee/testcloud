4c9a275e0e8486a2cd6b2480a58d0207572cb84a
František Zatloukal <fzatlouk@redhat.com>
2024-10-11 22:11:49 +0200

    Release 0.11.1

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -55,7 +55,7 @@ copyright = "2016-2024, testCloud devs"
 # built documents.
 #
 # The short X.Y version.
-version = "0.11.0"
+version = "0.11.1"
 # The full version, including alpha/beta/rc tags.
 release = version
 
--- testcloud/__init__.py
+++ testcloud/__init__.py
@@ -1,5 +1,5 @@
 # NOTE: if you update version, *make sure* to also update `docs/source/conf.py`
-__version__ = "0.11.0"
+__version__ = "0.11.1"
 
 import signal
 import sys
